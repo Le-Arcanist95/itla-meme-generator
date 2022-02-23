53ee83edc94cfab455f8b8fd3a9ca2345a536e43
Levi Arcane <zarcanist95@gmail.com>
2022-02-23 13:44:18 -0500

    Initial backend commit with State, API call, and update functions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,39 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 export default function App() {
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    imgUrl: ""
+  });
+  const [memeData, setMemeData] = useState([]);
+
+  useEffect(() => {
+    const getMemes = async () => {
+      const res = await fetch("https://api.imgflip.com/get_memes");
+      const data = await res.json()
+      setMemeData(data.data.memes)
+    };
+
+    getMemes();
+  }, []);
+  const newMeme = () => {
+    const randomNum = Math.floor(Math.random() * memeData.length);
+    const newUrl = memeData[randomNum].url;
+
+    setMeme(prevMeme => ({
+      ...prevMeme,
+      imgUrl: newUrl
+    }));
+  };
+  const handleChange = (event) => {
+    const {name, value} = event.target;
+    setMeme(prevMeme => ({
+      ...prevMeme,
+      [name]: value
+    }));
+  };
+
   return(
     <div>Meme Generator</div>
   )
