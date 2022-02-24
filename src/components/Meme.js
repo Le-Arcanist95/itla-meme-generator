24c23eb4017ca0a4662ebec44864062daf7cd993
Levi Arcane <zarcanist95@gmail.com>
2022-02-24 13:51:51 -0500

    Minor tweak on Meme.js removing redundant code

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -9,16 +9,16 @@ export default function Meme(props){
             <img alt="" src={props.imgUrl} className="savedMeme--img"/>
             {
                 isEditing ?
-                <button className="material-icons blue savedMeme--Icon" id={props.id} onClick={() => {
+                <button className="material-icons blue savedMeme--Icon" onClick={() => {
                     props.handleSave(props.id);
                     setIsEditing(false);
                 }}>save</button> : 
-                <button className="material-icons blue savedMeme--Icon" id={props.id} onClick={() => {
+                <button className="material-icons blue savedMeme--Icon" onClick={() => {
                     props.handleEdit(props.id);
                     setIsEditing(true)
                 }}>edit</button>
             }
-            <button className="material-icons blue savedMeme--eraseIcon" id={props.id} onClick={() => props.handleDelete(props.id)}>delete</button>
+            <button className="material-icons blue savedMeme--eraseIcon" onClick={() => props.handleDelete(props.id)}>delete</button>
         </div>
     )
 }
